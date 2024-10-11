e04800e48d706968ce977ac23c5f101a6db4a1a4
Liza Blomdahl-Olsevskaja <okaziya@mkd-mba.local>
2024-10-11 14:18:05 +0200

    feat: add next.js react project
    Resolves #3

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div>
+        <h1>Hello, Next.js!</h1>
+        <p>Welcome to your new Next.js project.</p>
+      </div>
+    );
+  }
