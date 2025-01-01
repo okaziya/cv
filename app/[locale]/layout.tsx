ee8d1beaf65de25c434ba68b32f1244e1cd93c65
okaziya <liza.blomdahl@gmail.com>
2025-01-01 16:40:46 +0100

    style: :art: use global styles, apply nunito sans font

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,10 +1,19 @@
 "useClient";
 
 import { getTranslations } from "../../lib/getTranslations";
+import GlobalStyle from "../../styles/GlobalStyles";
 
 export async function generateStaticParams() {
   return [{ locale: "en" }, { locale: "sv" }];
 }
+
+import { Nunito_Sans } from "next/font/google";
+const nunitoSans = Nunito_Sans({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-nunito",
+  subsets: ["latin"],
+});
 
 export default function LocaleLayout({
   children,
@@ -18,9 +27,10 @@ export default function LocaleLayout({
   const translations = getTranslations(locale);
 
   return (
-    <html lang={locale}>
+    <html lang={locale} className={nunitoSans.variable}>
       <body>
         <header>{translations.welcome}</header>
+        <GlobalStyle />
         {children}
         <footer>{translations.description}</footer>
       </body>
