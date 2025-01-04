ab299db3f1442c72c57b904e1cc66a64fba9b819
okaziya <liza.blomdahl@gmail.com>
2025-01-04 19:07:22 +0100

    fix: get rid of the Flash Of Unstyled Content

--- components/ThemeWrapper.tsx
+++ components/ThemeWrapper.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/theme";
 import GlobalStyles from "../styles/GlobalStyles";
@@ -9,6 +10,16 @@ export default function ThemeWrapper({
 }: {
   children: React.ReactNode;
 }) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
