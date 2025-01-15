66d3b0163f97438384ecf5411613fa0294841ed1
okaziya <liza.blomdahl@gmail.com>
2025-01-15 16:37:19 +0100

    style: :art: make header responsive

--- components/Header/Header.styles.tsx
+++ components/Header/Header.styles.tsx
@@ -1,8 +1,27 @@
 "use client";
 
 import styled from "styled-components";
+import { media } from "../../styles/media";
 
 export const StyledHeader = styled.header`
   max-width: ${({ theme }) => theme.breakpoints.desktop};
   height: 60px;
+
+  ${media.mobile`
+    height: 76px;
+  `}
+`;
+
+export const ImageWrapper = styled.div`
+  width: 100%;
+  max-width: 80px;
+  margin-bottom: 12px;
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  ${media.mobile`
+      max-width: 56px; 
+  `}
 `;
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { getTranslations } from "../../lib/getTranslations";
 import { Locale } from "../../types";
 import Image from "next/image";
 import Button from "../Button";
-import { StyledHeader } from "./Header.styles";
+import { ImageWrapper, StyledHeader } from "./Header.styles";
 import { CONTACT_INFO } from "../../config";
 import { getImagePath } from "../../utils/imagePath";
 
@@ -13,14 +13,16 @@ export default function Header({ locale }: { locale: Locale }) {
   return (
     <StyledHeader className="mx-auto d-flex justify-content-between">
       <div className="d-flex flex-column justify-content-between">
-        <Image
-          src={getImagePath("/glasses.png")}
-          alt={translations.images.logoAlt}
-          width={80}
-          height={16}
-          priority
-          unoptimized
-        />
+        <ImageWrapper>
+          <Image
+            src={getImagePath("/glasses.png")}
+            alt={translations.images.logoAlt}
+            width={80}
+            height={16}
+            priority
+            unoptimized
+          />
+        </ImageWrapper>
         <h3 className="m-0">{CONTACT_INFO.name}</h3>
       </div>
       <div className="d-flex">
