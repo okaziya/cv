7af742b9c7c3e78bdadb4c4021c544fbc4b05cee
okaziya <liza.blomdahl@gmail.com>
2025-02-10 18:43:35 +0100

    feat: :tada: adding consulting friendly description for the iQuest

--- locales/en.ts
+++ locales/en.ts
@@ -58,9 +58,59 @@ const en = {
         companyTitle: "iQuest s.r.o. – Prague, Czech Republic",
         date: "2019-06–2021-10",
         highlights:
-          "- Specialized in front-end development with additional experience in back-end development.\n" +
+          "- Specialized in front-end development at a software company, with additional experience in back-end development.\n" +
           "- Developed responsive styling for web and mobile projects.\n" +
           "- Worked on diverse projects using various programming languages and libraries.\n",
+        adminApplication:
+          "Developed both the frontend and backend for an admin panel, integrating Firebase as the backend service.\n" +
+          "Designed and implemented features for saving, retrieving, and organizing structured data within Firebase, ensuring efficient data handling and real-time updates.\n" +
+          "Implemented user authentication using Firebase Authentication with Google Sign-In, ensuring secure access control.\n" +
+          "Handled authentication, database interactions, and synchronization between frontend components and Firebase services.\n" +
+          "Ensured seamless communication between the user interface and backend logic to provide a smooth user experience.\n" +
+          "Technologies: JavaScript, Firebase (Firestore, Realtime Database), Git.",
+        iQuestWeb:
+          "Participating in updating the design while refactoring the styling code for iQust on both web and mobile screens.\n" +
+          "Rewriting CSS into SCSS to improve code reusability and maintainability.\n" +
+          "Removing unused code and adding nesting for better readability.\n" +
+          "Improving and ensuring consistent class naming.\n" +
+          "Organizing the positioning of containers and wrappers throughout the application.\n" +
+          "Enhancing parameters such as Performance, Accessibility, and SEO.\n" +
+          "Replacing jQuery with vanilla JavaScript.\n" +
+          "Adding various CSS animations.\n" +
+          "Developing new pages.\n" +
+          "HTML, GIT, SCSS, JavaScript, Ruby.\n",
+        farmacyCompany:
+          "Pharmacy Platform:\n" +
+          "The frontend development for a pharmacy company’s platform.\n" +
+          "Implemented and customized Bootstrap, rewriting variables and applying a custom theme for a unique UI.\n" +
+          "Ensured responsiveness and improved user experience across devices.\n" +
+          "Worked in constant collaboration with backend developers and designer to align functionality and aesthetics.\n" +
+          "Technologies: Bootstrap, JavaScript, CSS, GIT.\n",
+        groceryWebProject:
+          "Grocery Web Platform:\n" +
+          "Built a Jekyll-based website for a grocery brand to showcase products.\n" +
+          "Included product descriptions, images, and adjusted recipes tailored to specific products.\n" +
+          "Ensured a responsive and user-friendly design to enhance the shopping experience.\n" +
+          "Technologies: Jekyll, HTML, CSS, JavaScript, GIT.\n",
+        MeduHelp:
+          "Implementing of design for custom video player for an educational platform focused on first aid training.\n" +
+          "Challenges included creating custom UI controls.\n" +
+          "Optimized performance, improved accessibility, and implemented interactive quizzes.\n" +
+          "Technologies: HTML5 Video API, JavaScript, CSS.\n",
+        groceryShopProject:
+          "Developed the frontend of a React Native mobile app for a grocery store.\n" +
+          "Helped customers view sales and accumulate bonuses.\n" +
+          "Using Android Studio for testing and debugging.\n" +
+          "Worked closely with backend developers to ensure smooth integration and user experience.\n" +
+          "Technologies: React Native, JavaScript, Android Studio, GIT.\n",
+        doosanProject:
+          "Full implementation of the design for the Doosan desktop application for offline access to a large number of files.\n" +
+          "Development from scratch till completion.\n" +
+          "Interesting frontend features:\n" +
+          "- Progress bar with the state of the currently downloaded files.\n" +
+          "- Numerous subcategories and adding a search.\n" +
+          "- Some details required me to create the design myself.\n" +
+          "Technologies: React, GIT, CSS.\n",
       },
     ],
   },
